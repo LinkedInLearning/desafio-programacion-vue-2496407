1de5c0cbcf5f966b295281032a350c2a51a5c840
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-22 23:30:24 +0100

    final

---
+++ desafio01/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1 class="py-3">Lista de pendientes</h1>
+        <ul class="list-group mb-3">
+          <li class="list-group-item" v-for="(item, index) in pendientes" v-bind:key="index">
+            <div class="row">
+              <div class="col">{{item}}</div>
+              <div class="col"><button type="button" class="btn btn-secondary btn-sm" @click="removerTarea(index)">Hecho!</button></div>
+            </div>
+          </li>
+        </ul>
+        <div class="card px-5 py-5">
+          <h2>Agrega más pendientes</h2>
+          <div class="row">
+            <div class="col">
+              <input class="form-control" type="text" placeholder="Hacer la cena..." ref="textoTarea"/>
+            </div>
+            <div class="col">
+              <button type="button" class="btn btn-primary" @click="agregarTarea">Agregar a lista</button>
+            </div>
+          </div>
+        </div>
+        </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ListaPendientes',
+  data: ()=> ({
+    pendientes: ['Hacer la cena']
+  }),
+  methods: {
+    removerTarea(index){
+      this.pendientes.splice(index, 1);
+    },
+    agregarTarea(){
+      this.pendientes.push(this.$refs.textoTarea.value);
+      this.$refs.textoTarea.value = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+}
+</style>
