28255c35bad3b587c70a86aa30f3390f4e4e6c8f
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-29 04:30:11 +0100

    final

---
+++ desafio03/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <PerfilUsuario
+  v-for="perfil in perfiles"
+  :perfil="perfil"
+  v-bind:key="perfil.id"/>
+</template>
+
+<script setup>
+import PerfilUsuario from './components/PerfilUsuario.vue'
+import perfiles from './assets/usuarios.json'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ desafio03/src/components/PerfilUsuario.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{$props.perfil.perfil.nombre}}</h5>
+    <div class="card-text">
+      <p><span class="fw-bold">Email: </span>{{$props.perfil.email}}</p>
+      <p><span class="fw-bold">Dirección: </span>{{$props.perfil.perfil.direccion}}</p>
+      <p><span class="fw-bold">Acerca: </span>{{$props.perfil.perfil.acerca}}</p>
+    </div>
+    <a href="#" class="btn btn-primary">Contactar</a>
+  </div>
+</div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['perfil']
+  }
+  </script>
