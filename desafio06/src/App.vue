539254f399298cef84a822300c20525ca404805d
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-30 20:33:13 +0100

    amend

--- desafio06/src/App.vue
+++ desafio06/src/App.vue
@@ -1,39 +1,14 @@
 <template>
   <nav class="navbar">
-  <div class="container">
-    <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    {{elementosPerfil[indexSelecc].label}}
-  </button>
-  <ul class="dropdown-menu">
-    <li
-      v-for="(el, index) in elementosPerfil"
-      v-bind:key="index"
-      @click="seleccionarElemento(index)"
-    >
-      <a class="dropdown-item" href="#">{{el.label}}</a>
-    </li>
-  </ul>
-</div>
-
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Orden: {{orden}}
-  </button>
-  <ul class="dropdown-menu">
-    <li @click="seleccionarOrden('asc')"><a class="dropdown-item" href="#">Ascendente</a></li>
-    <li @click="seleccionarOrden('desc')"><a class="dropdown-item" href="#">Descendente</a></li>
-  </ul>
-</div>
-
-  </div>
+    <div class="container">
+      <div class="input-group flex-nowrap">
+        <span class="input-group-text" id="addon-wrapping">Buscar</span>
+        <input type="text" class="form-control" v-model="busqueda">
+      </div>
+    </div>
   </nav>
 
-  <PerfilUsuario
-    v-for="perfil in listaOrdenada"
-    :perfil="perfil"
-    v-bind:key="perfil.id"
-  />
+  <PerfilUsuario v-for="perfil in listaFiltrada" :perfil="perfil" v-bind:key="perfil.id" />
 </template>
 
 <script setup>
@@ -41,32 +16,22 @@ import { ref, computed } from 'vue'
 import PerfilUsuario from './components/PerfilUsuario.vue'
 import perfiles from './assets/usuarios.json'
 
-const elementosPerfil = [
-    {key: 'email', label: 'Correo'},
-    {key: 'usuario', label: 'Usuario'},
-    {key: 'nombre', label: 'Nombre'},
-    {key: 'compania', label: 'Compañia'},
-    {key: 'f_nacimiento', label: 'Fecha de nacimiento'},
-  ];
+const busqueda = ref('');
 
-const indexSelecc = ref(0);
-const orden = ref('asc');
+const listaFiltrada = computed(() => {
+  return perfiles.filter(perfil => {
+    
+    const regex = new RegExp(busqueda.value, 'i');
+    
+    const email = perfil.email.search(regex) > -1;
+    const usuario = perfil.usuario.search(regex) > -1;
+    const nombre = perfil.nombre.search(regex) > -1;
+    const compania = perfil.compania.search(regex) > -1;
+    const direccion = perfil.direccion.search(regex) > -1;
+    const acerca = perfil.acerca.search(regex) > -1;
 
-const seleccionarElemento = (index)=>{
-  indexSelecc.value = index;
-}
-
-const seleccionarOrden = (ordenSeleccionado)=>{
-  orden.value = ordenSeleccionado;
-}
-
-const listaOrdenada = computed(() => {
-  return perfiles.sort((a, b) => {
-    if(orden.value === 'asc'){
-      return ('' + a[elementosPerfil[indexSelecc.value].key]).localeCompare(b[elementosPerfil[indexSelecc.value].key]);
-    }
-    return ('' + b[elementosPerfil[indexSelecc.value].key]).localeCompare(a[elementosPerfil[indexSelecc.value].key]);
-})
+    return email || usuario || nombre || compania || direccion || acerca;
+  })
 })
 
 
