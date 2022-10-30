264257bbb78230af0d64517aa011a902a77e9022
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-30 22:20:37 +0100

    first

---
+++ desafio09/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#miCuandroDialogo">
+    Abrir cuandro de diálogo
+  </button>
+  <VentanaDialogo target-id="miCuandroDialogo"/>
+</template>
+
+<script>
+import VentanaDialogo from './components/VentanaDialogo.vue';
+export default {
+  name: 'App',
+  components: { VentanaDialogo }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
