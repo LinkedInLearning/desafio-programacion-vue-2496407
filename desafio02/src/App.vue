9815c61cdf9245d94dcab05f3365fab2b5a56941
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-31 16:35:48 +0100

    amend

--- desafio02/src/App.vue
+++ desafio02/src/App.vue
@@ -1,36 +1,26 @@
 <template>
-  <div class="row mt-5 ml-5">
-    <div class="col">
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
       <div class="btn-group" role="group">
-        <button type="button" class="btn btn-primary" @click="saltar">Saltar</button>
-        <button type="button" class="btn btn-secondary" @click="correr">Correr</button>
+        <button type="button" class="btn btn-primary" @click="() => tipoAnimacion = 'saltar'">Saltar</button>
+        <button type="button" class="btn btn-secondary" @click="() => tipoAnimacion = 'correr'">Correr</button>
+      </div>
+      <div class="btn-group" role="group">
+        <button type="button" class="btn btn-primary" @click="() => velocidad -= 0.2">Aumentar velocidad</button>
+        <button type="button" class="btn btn-secondary" @click="() => velocidad += 0.2">Disminuir velocidad</button>
       </div>
     </div>
-  </div>
-  <div ref="figura" id="circulo"></div>
+  </nav>
+
+  <FiguraCuadrado :movimiento="tipoAnimacion" :velocidad="velocidad" />
 </template>
 
 <script setup>
-import gsap from 'gsap'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
+import FiguraCuadrado from './components/FiguraCuadrado';
 
-const figura = ref(null)
-
-const correr = () => {
-      gsap.killTweensOf(figura.value);
-      gsap.set(figura.value, {x: 0, y: window.innerHeight / 2});
-      gsap.to(figura.value,
-      {x: window.innerWidth, duration: 5, repeat: -1});
-}
-
-const saltar = () => {
-      gsap.killTweensOf(figura.value);
-      gsap.set(figura.value, {x: window.innerWidth / 2, y: window.innerHeight - 400});
-      gsap.to(figura.value,
-      {y: 0, duration: 2, repeat: -1, yoyo: true});
-    }
-
-onMounted(()=> correr());
+const tipoAnimacion = ref('saltar');
+const velocidad = ref(5);
 
 </script>
 
@@ -40,15 +30,5 @@ onMounted(()=> correr());
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-#circulo{
-  width: 100px;
-  height: 100px;
-  background-color: aqua;
-  border-radius: 100px;
-  border: dotted 5px #2c3e50;
-  position: absolute;
-  top: 300px;
-  left: 10px;
 }
 </style>
