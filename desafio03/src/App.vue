b82097d9e48b16ee6243ea84c8526771cb4cb0c7
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2022-10-31 15:24:23 +0100

    amend

--- desafio03/src/App.vue
+++ desafio03/src/App.vue
@@ -1,12 +1,8 @@
 <template>
-  <PerfilUsuario
-  v-for="perfil in perfiles"
-  :perfil="perfil"
-  v-bind:key="perfil.id"/>
+
 </template>
 
 <script setup>
-import PerfilUsuario from './components/PerfilUsuario.vue'
 import perfiles from './assets/usuarios.json'
 </script>
 
