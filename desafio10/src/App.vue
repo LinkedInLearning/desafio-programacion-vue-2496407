1cfa2d62b21b9084a55490118089885f97ea5b6c
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2022-11-01 16:21:16 +0100

    first

--- desafio10/src/App.vue
+++ desafio10/src/App.vue
@@ -1,37 +1,13 @@
 <template>
-  <h3 v-tiempo>2022-11-01 08:30:00.12</h3>
-  <h3 v-tiempo>2022-10-31 08:30:00.12</h3>
-  <h3 v-tiempo>2022-10-30 08:30:00.12</h3>
-  <h3 v-tiempo>2022-10-29 08:30:00.12</h3>
+  <h3>2022-11-01 08:30:00.12</h3>
+  <h3>2022-10-31 08:30:00.12</h3>
+  <h3>2022-10-30 08:30:00.12</h3>
+  <h3>2022-10-29 08:30:00.12</h3>
 </template>
 
 <script setup>
 import moment from 'moment'
 
-const vTiempo = {
-  created: (el) => formatoFecha(el),
-  updated: (el) => formatoFecha(el)
-}
-
-const formatoFecha = (el) => {
-  const ahora = moment(new Date()); //fecha actual
-  const fechaTexto = moment(el.innerText.trim()); // fecha del texto
-  const duracion = moment.duration(ahora.diff(fechaTexto));
-  const horas = duracion.asHours();
-
-  if( horas < 24){
-    el.innerText = `Hace ${Math.floor(horas)} horas`;
-    return;
-  }
-
-  if(horas >= 24 && horas <= 48){
-    el.innerText = 'Ayer';
-    return;
-  }
-
-  el.innerText = `Hace  ${Math.floor(horas / 24)} días`;
-
-}
 </script>
 
 <style>
