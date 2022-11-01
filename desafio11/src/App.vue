217d9e83131933f8d37ef13b857397748709f112
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-11-01 20:43:23 +0100

    first

---
+++ desafio11/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#miCuandroDialogo">
+    Abrir cuandro de diálogo
+  </button>
+  <VentanaDialogo target-id="miCuandroDialogo">
+    <template v-slot:header>
+      <h3>Cabecera del diálogo</h3>
+    </template>
+    <template v-slot:body>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id dolor diam. Fusce sem augue, consequat quis
+        sapien vitae, ultrices imperdiet eros. Vivamus posuere dui eget auctor vestibulum. Curabitur tincidunt eros
+        turpis, sed convallis leo convallis venenatis.</p>
+    </template>
+    <template v-slot:footer>
+      <button type="button" class="btn btn-primary btn-lg">Aceptar</button>
+    </template>
+  </VentanaDialogo>
+</template>
+
+<script>
+import VentanaDialogo from './components/VentanaDialogo.vue';
+export default {
+  name: 'App',
+  components: { VentanaDialogo }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ desafio11/src/components/VentanaDialogo.vue
@@ -0,0 +1,31 @@
+<template>
+  <Teleport to="body">
+    <div class="modal fade" v-bind:id="targetId" tabindex="-1" aria-labelledby="ejemploDialogo" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <slot name="header"></slot>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <slot name="body"></slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</Teleport>
+</template>
+
+<script>
+export default {
+  name: 'VentanaDialogo',
+  props: {
+    targetId: String
+  }
+}
+</script>
+
+<style scoped></style>
