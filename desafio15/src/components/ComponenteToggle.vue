41488859a17147a85f12b3cf89ca8b1a99a950cc
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2022-11-02 14:40:54 +0100

    first

--- desafio15/src/components/ComponenteToggle.vue
+++ desafio15/src/components/ComponenteToggle.vue
@@ -1,24 +1,12 @@
 <template>
   <div class="form-check form-switch">
-    <input class="form-check-input" type="checkbox" role="switch" id="toggle" v-model="valor">
-    <label class="form-check-label" for="toggle">{{props.label}}</label>
+    <input class="form-check-input" type="checkbox" role="switch" id="toggle">
+    <label class="form-check-label" for="toggle"></label>
   </div>
 </template>
 
 <script setup>
 import { defineProps, defineEmits, ref, watch } from 'vue';
-
-const props = defineProps({
-  modelValue: Boolean,
-  label: String
-});
-
-const emits = defineEmits(['update:modelValue']);
-
-const valor = ref()
-
-watch(() => props.modelValue, () => valor.value = props.modelValue);
-watch(() => valor.value, () => emits('update:modelValue', valor.value));
 
 </script>
 
