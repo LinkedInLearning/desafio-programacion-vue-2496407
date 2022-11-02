f8096eac57c414bf149a3a079630f2a8070982d0
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-11-02 10:01:47 +0100

    first

---
+++ desafio15/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container mt-5">
+    <div class="row justify-content-around">
+      <div class="col-2">
+        <p>valorToggle: {{valorToggle}}</p>
+        <ComponenteToggle v-model="valorToggle" label="Prueba Toggle"/>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ComponenteToggle from './components/ComponenteToggle'
+
+const valorToggle = ref(false);
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ desafio15/src/components/ComponenteToggle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="form-check form-switch">
+    <input class="form-check-input" type="checkbox" role="switch" id="toggle" v-model="valor">
+    <label class="form-check-label" for="toggle">{{props.label}}</label>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, ref, watch } from 'vue';
+
+const props = defineProps({
+  modelValue: Boolean,
+  label: String
+});
+
+const emits = defineEmits(['update:modelValue']);
+
+const valor = ref()
+
+watch(() => props.modelValue, () => valor.value = props.modelValue);
+watch(() => valor.value, () => emits('update:modelValue', valor.value));
+
+</script>
+
+<style scoped>
+
+</style>
