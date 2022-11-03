4843f00c6990cf429e146cd68db6e9c9d2282214
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-11-03 15:04:38 +0100

    first

---
+++ desafio19/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="circulo" ref="circulo3"></div>
+  <div class="circulo" ref="circulo2"></div>
+  <div class="circulo" ref="circulo1"></div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import gsap from 'gsap'
+
+const circulo1 = ref();
+const circulo2 = ref();
+const circulo3 = ref();
+const escala = ref();
+
+const generarAleatorio = (min, max) =>{
+    return Math.random() * (max - min + 1) + min
+}
+
+const animar = () =>{
+  escala.value = generarAleatorio(0.5, 2);
+  gsap.to(circulo1.value, { scale: escala.value, duration: 1, delay: 1, onComplete: () => {
+    gsap.to(circulo1.value, { scale: 0, duration: 1, delay: 0.5, onComplete: animar});
+  }});
+}
+
+const animar2 = () => {
+  gsap.to(circulo2.value, { scale: escala.value + 0.5, duration: 1, delay: 1, onComplete: () => {
+    gsap.to(circulo2.value, { scale: 0, duration: 1, delay: 0.5});
+  }});
+}
+
+const animar3 = () => {
+  gsap.to(circulo3.value, { scale: escala.value + 1, duration: 1, delay: 1, onComplete: () => {
+    gsap.to(circulo3.value, { scale: 0, duration: 1, delay: 0.5});
+  }});
+}
+
+watch(escala, ()=> animar2());
+
+watch(escala, ()=> animar3());
+
+onMounted(() => {
+  animar();
+})
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.circulo {
+  transform-origin: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 300px;
+  border: 2px dotted black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+</style>
